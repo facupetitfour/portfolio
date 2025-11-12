754e40e17698d9ed78b5833c7f7880d1ba96b589
facupetitfour <facupetitfour@gmail.com>
2025-11-12 20:03:31 -0300

    arreglos 4.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://facundo-petitfour.github.io',
+  site: 'https://facupetitfour.github.io/portfolio',
   base: '/',
   vite: {
     plugins: [tailwindcss()],
